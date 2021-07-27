26501378b2ed5231d46abce39b02d1dc4bc67970
Tim Ross <timr894@gmail.com>
2021-07-26 20:26:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ require('dotenv').config();
 
 
 //tells node to run on port 3000 and when it gets a 200 to log that is listening 
-app.listen(3000, () => console.log('listening at port 3000')); 
+const port = process.env.PORT || 3000; 
+app.listen(port, () => console.log(`listening at port ${port}`)); 
 // tells express to run the files in the public folder
 // new static direction rather than public file I updated to just use the directory
 app.use(express.static(__dirname)); 
